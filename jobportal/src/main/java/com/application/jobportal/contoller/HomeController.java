67046faa2388ba6303e4ae6a0c2fa8e0bb5d23ef
bala873 <bala.v873@gmail.com>
2024-11-22 16:28:29 +0530

    first commit.

---
+++ jobportal/src/main/java/com/application/jobportal/contoller/HomeController.java
@@ -0,0 +1,13 @@
+package com.application.jobportal.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+}
